781fa6df49ecfa5ab820e849f5bbb7b19d34563b
Leandro <leandrorullo@gmail.com>
2023-01-17 21:01:10 +0100

    3er Preentrega Rullo

---
+++ README.md
@@ -0,0 +1,10 @@
+proyectoVivero
+Nombre del proyecto:
+
+Vivero Online
+
+Descripcion:
+
+Tienda online de productos y servicios relacionados a Plantas y jardineria. Tambien se administra la carga de productos, stock, y descripciones. Registro de Usuarios y Log in.
+
+Tecnologias: -Django 4.1.4 -Python 3.10 -HTML5 -CSS -JS
--- products/urls.py
+++ products/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
      path('lista-Plantin/', listarPlantin, name='listaplantin'),
      path('lista-Arbol/', listarArbol, name='listaarbol'),
      path('lista-Frutal/', listarFrutal, name='listafrutal'),
-     path('Buscar-Arbol/',busca_arboles, name= 'buscaarbol'),
-     path('Buscar-Frutal/',busca_frutales, name= 'buscafrutal'),
-     path('Buscar-Plantin/',busca_plantin, name= 'buscaplantin'),
+     path('Buscar-Arbol/',busca_arboles, name= 'buscaarbol'),# type: ignore  
+     path('Buscar-Frutal/',busca_frutales, name= 'buscafrutal'),# type: ignore  
+     path('Buscar-Plantin/',busca_plantin, name= 'buscaplantin'),# type: ignore  
 ]
